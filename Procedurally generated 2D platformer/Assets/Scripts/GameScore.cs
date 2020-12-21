3289ddac8bc4bd033244e57138899435a0b6206b
Marin Polgar <mp49404@fer.hr>
2020-12-21 16:16:59 +0100

    Added

---
+++ Procedurally generated 2D platformer/Assets/Scripts/GameScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameScore : MonoBehaviour
+{
+    [SerializeField, Tooltip("The character prefab for player position.")]
+    private GameObject playerCharacter;
+
+    private TMP_Text text;
+    
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+        text.text = "0";
+    }
+    
+    void Update()
+    {
+        text = GetComponent<TMP_Text>();
+        text.text = playerCharacter.GetComponent<Character>().score.ToString();
+    }
+}
